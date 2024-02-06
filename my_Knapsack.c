8c11b37b7a59682331e13c4e2de23095124b2a93
eilon <eilonashwal30@gmail.com>
2024-02-06 20:08:23 +0200

    final eilon

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -5,27 +5,42 @@ int main() {
 
    int weights[Array_size];
    int values[Array_size] ;
-   int selected_bool[Array_size];
    int result[Array_size];
 
+   // Initialize  the result array with 0
+    for (int i = 0; i < Array_size; i++)
+        result[i] = 0;
 
     // the items are represented by the following array
-    int item[] = {'A', 'B', 'C', 'D', 'E'};
+    int items[] = {'A', 'B', 'C', 'D', 'E'};
+
+    // Get input from user to fill the values array
+    for (int i = 0; i < Array_size; i++) {
+        scanf("%d", &values[i]);
+    }
 
     // Get input from user to fill the weights array
     for (int i = 0; i < Array_size; i++) {
         scanf("%d", &weights[i]);
     }
-    // Get input from user to fill the values array
-    for (int i = 0; i < Array_size; i++) {
-        scanf("%d", &values[i]);
-    }
     
     // print the maximum profit
-    printf("Maximum profit: %d", knapSack(weights, values, selected_bool));
+    printf("Maximum profit: %d\n", knapSack(weights, values, result));
+
 
     //print the items that give the maximum profit
-    printf("Items that give the maximum profit: %s", result);
+    printf("Items that give the maximum profit: [");
+
+    int printed = 0;
+    for (int i = 0; i < Array_size; i++) {
+        if (result[i]) {
+            if (printed > 0)
+                printf(", ");
+            printf("%c", items[i]);
+            printed++;
+        }
+    }
+    printf("]\n");
     
     return 0;
 }
--- my_mat.c
+++ my_mat.c
@@ -35,3 +35,44 @@ void floyd_Warshall (int matrix[SIZE][SIZE]){
     }
 
 }
+
+// Function to find the maximum of two integers
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+// Function to solve the 0-1 knapsack problem
+int knapSack(int weights[], int values[], int included[]) {
+    int i, w;
+    int dp[ Array_size+ 1][MAX_WEIGHT + 1];
+
+    // Build the dp table bottom up
+    for (i = 0; i <= Array_size; i++) {
+        for (w = 0; w <= MAX_WEIGHT; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    // Trace back to find the items included
+    int res = dp[Array_size][MAX_WEIGHT];
+    w = MAX_WEIGHT;
+    for (i = Array_size; i > 0 && res > 0; i--) {
+        // If the result comes from the top (previous item) or not
+        if (res == dp[i - 1][w])
+            continue;
+        else {
+            // This item is included
+            included[i - 1] = 1;
+            res -= values[i - 1];
+            w -= weights[i - 1];
+        }
+    }
+
+    return dp[Array_size][MAX_WEIGHT];
+}
+
